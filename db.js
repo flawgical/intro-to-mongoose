46385d91b61ada5b3d26172f03f5432e8f79c7ea
Shahzad <shahzad.khan@generalassemb.ly>
2018-07-12 16:15:04 -0500

    created a collection of article, showed query methods of find(), and update()

--- db.js
+++ db.js
@@ -15,3 +15,33 @@ db.on('error', (err) => {
 db.on('connected', () => {
   console.log('mongoose is connected to mongodb')
 });
+
+// Article.create(
+//   {
+//     title: 'Python',
+//     author: 'Shahzad'
+//   },
+//   (err, article) => {
+//     //first argument is an error message from mongo
+//     // second is the document you created and you can all it whatever
+//     if(err) {
+//       console.log(err)
+//     } else {
+//       console.log(article);
+//     }
+//   }
+// )
+
+
+//first argument is what we want to find by,
+// Article.find({author:'Shahzad'}, (err, articles) => {
+//   console.log(articles) //will return an array;
+// })
+
+
+Article.update(
+    {author: 'Shahzad'}, //thing it update
+    { $set: {author: 'Shahzad Khan'}}, //object with property
+    {multi: true} //this is the options. Multi true says update all
+    // things that are Shahzad to Shahzad Khan
+)
